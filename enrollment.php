c0f30cc619edaa024ab585029f1f690aae32a652
shonalpas <147411186+shonalpas@users.noreply.github.com>
2023-12-19 14:32:09 +0800

    Add files via upload

---
+++ enrollment.php
@@ -0,0 +1,13 @@
+<?php
+    $studentName = $_POST['studentNAME'];
+    $birthdate = $_POST['birthdate'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $previousSchool = $_POST['previousSchool'];
+    $prefferedCourse = $_POST['prefferedCourse'];
+    $healthInfo = $_POST['healthInfo'];
+
+
+?>
